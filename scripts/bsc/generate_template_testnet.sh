5fa5ac844a67b890a12ec29fbd9de444ec90821d
Myroslav Gavryliak <myroslav.gavriliak@gmail.com>
2022-09-21 22:16:21 +0300

    wip: fix for boken block 22688842

--- scripts/bsc/generate_template_testnet.sh
+++ scripts/bsc/generate_template_testnet.sh
@@ -5,6 +5,7 @@ set -e
 # prepare template 
 rm ./subgraph.yaml || true 
 cp ./subgraph_template.yaml ./subgraph.yaml 
-sed -i '' -e "s/{{ADDRESS}}/0xF7cAD1f9De025022BB370Bc59D9B2f4cE507771d/" ./subgraph.yaml 
-sed -i '' -e "s/{{START_BLOCK}}/17393453/" ./subgraph.yaml
+sed -i '' -e "s/{{ADDRESS}}/0xf39d00f8ad68bcc4eca81ed87c62427c114321af/" ./subgraph.yaml 
+# sed -i '' -e "s/{{START_BLOCK}}/21478131/" ./subgraph.yaml
+sed -i '' -e "s/{{START_BLOCK}}/22688843/" ./subgraph.yaml
 sed -i '' -e "s/{{CHAIN}}/chapel/" ./subgraph.yaml
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,104 +1,110 @@
-import { bigInt, BigInt, json, JSONValue } from "@graphprotocol/graph-ts"
+import { json, log } from "@graphprotocol/graph-ts";
 import {
-  NFT,
   Approval,
-  ApprovalForAll,
-  Transfer,
-  buyed
-} from "../generated/NFT/NFT"
-import { NFTToken, User } from "../generated/schema"
+  ApprovalForAll, buyed, NFT, Transfer
+} from "../generated/NFT/NFT";
+import { NFTToken, User } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract._tokenIds(...)
-  // - contract.balanceOf(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.minero(...)
-  // - contract.nTokenOnSale(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.tokensData(...)
-  // - contract.totalSupply(...)
-  // - contract.minar(...)
-  // - contract.transferirNft(...)
-  // - contract.quemarNft(...)
-  // - contract.obtenerNfts(...)
-  // - contract.obtenerPaginav1(...)
-  // - contract.obtenerPaginav2(...)
-  // - contract.tokensOf(...)
-  // - contract.tokensOfPaginav1(...)
-  // - contract.getItemInfo(...)
-  // - contract.obtenerNftsbyrango(...)
-}
-
+export function handleApproval(event: Approval): void {}
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleTransfer(event: Transfer): void {
-  let token = NFTToken.load(event.params.tokenId.toString());
-  if (!token) {
-    token = new NFTToken(event.params.tokenId.toString());
-    
-    let tokenContract = NFT.bind(event.address);
-    const tokenInfo = tokenContract.getItemInfo(event.params.tokenId)[0];
+  log.info("here main {}", ['1']);
 
-    token.creator = tokenInfo.creator.toHexString();
-    token.token_id = tokenInfo.tokenID;
+  const tokenId = event.params.tokenId.toString();
+  if (!tokenId) {
+    log.info("skipping token id {}", ['1']);
+    return;
+  }
+
+  if (!event || !event.params || !event.params.tokenId) {
+    log.info("here second {}", ['1']);
+    return;
+  }
+
+  log.info("here token id {}", [tokenId]);
+  let token = NFTToken.load(tokenId);
+  if (!token) {
+    log.info("here {}", ['1']);
+    token = new NFTToken(event.params.tokenId.toString());
+
+    if (!event.address) {
+      log.info("here address {}", ['1']);
+      return;
+    }
+
+    let tokenContract = NFT.bind(event.address);
+    const tokenInfoWrapped = tokenContract.try_getItemInfo(event.params.tokenId);
+    log.info("length: {}; reverted: {}", [tokenInfoWrapped.value.length.toString(), tokenInfoWrapped.reverted.toString()]);
+    if (!tokenInfoWrapped.value.length && !tokenInfoWrapped.reverted) {
+      // in case of no data just skip it
+      log.info("No data for token {}", [event.params.tokenId.toString()]);
+      return;
+    }
+    const tokenInfoUnwrapped = tokenInfoWrapped.value.at(0);
+
+    if (tokenInfoUnwrapped.creator) {
+      token.creator = tokenInfoUnwrapped.creator.toHexString();
+    }
+      
+    if (tokenInfoUnwrapped.tokenID) {
+      token.token_id = tokenInfoUnwrapped.tokenID;
+    }
+
     const owner = tokenContract.ownerOf(token.token_id);
 
     // when new token generated creator is owner
     token.owner = owner.toHexString();
     // initially token is on sale
   
-    token.price = tokenContract.getItemInfo(event.params.tokenId)[0].price;
-    token.json = tokenContract.getItemInfo(event.params.tokenId)[0].data;
-    token.on_sale = tokenContract.getItemInfo(event.params.tokenId)[0].onSale;
-    
-    const data = json.fromString(tokenContract.getItemInfo(event.params.tokenId)[0].data);
-    const title = data.toObject().get("title");
-    if (title)  {
-      token.title = title.toString();
+    if (tokenInfoUnwrapped.price) {
+      token.price = tokenInfoUnwrapped.price;
     }
 
-    const description = data.toObject().get("description");
-    if (description) {
-      token.description = description.toString();
+    if (tokenInfoUnwrapped.onSale) {
+      token.on_sale = tokenInfoUnwrapped.onSale;
     }
 
-    const media = data.toObject().get("media");
-    if (media) {
-      token.media = media.toString();
+    if (tokenInfoUnwrapped.data) {
+      token.json = tokenInfoUnwrapped.data;
+      const data = json.fromString(tokenInfoUnwrapped.data);
+      const title = data.toObject().get("title");
+      if (title)  {
+        token.title = title.toString();
+      }
+  
+      const description = data.toObject().get("description");
+      if (description) {
+        token.description = description.toString();
+      }
+  
+      const media = data.toObject().get("media");
+      if (media) {
+        token.media = media.toString();
+      }
     }
 
     token.last_change = event.block.timestamp;
   } else {
+    log.info("here not {}", ['1']);
     let tokenContract = NFT.bind(event.address);
-    const tokenInfo = tokenContract.getItemInfo(event.params.tokenId)[0];
+    log.info("here not {}", ['2']);
+    const tokenInfoWrapped = tokenContract.try_getItemInfo(event.params.tokenId);
+    // log.info("length: {}; reverted: {}", [tokenInfoWrapped.value.length.toString(), tokenInfoWrapped.reverted.toString()]);
+    log.info("reverted: {}", [tokenInfoWrapped.reverted.toString()]);
+    // if (!tokenInfoWrapped.value.length && !tokenInfoWrapped.reverted) {
+    if (!tokenInfoWrapped.reverted) {
+      // in case of no data just skip it
+      log.info("No data for token {}", [event.params.tokenId.toString()]);
+      return;
+    }
+    const tokenInfoUnwrapped = tokenInfoWrapped.value.at(0);
     const owner = tokenContract.ownerOf(token.token_id);
 
     // when new token generated creator is owner
     token.owner = owner.toHexString();
-    token.on_sale = tokenContract.getItemInfo(event.params.tokenId)[0].onSale;
-    token.price = tokenContract.getItemInfo(event.params.tokenId)[0].price;
+    token.on_sale = tokenInfoUnwrapped.onSale;
+    token.price = tokenInfoUnwrapped.price;
     token.last_change = event.block.timestamp;
   }
   token.save();
@@ -108,9 +114,6 @@ export function handleTransfer(event: Transfer): void {
     user = new User(event.params.to.toHexString());
     user.save();
   }
-
 }
 
-export function handlebuyed(event: buyed): void {
-
-}
+export function handlebuyed(event: buyed): void {}
