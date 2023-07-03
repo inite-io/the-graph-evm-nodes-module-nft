68b2ca0012883ab38764cd4d21b140162f0c252a
Andrei Ivanouski <aivanouski@gmail.com>
2023-07-03 18:10:58 +0800

    update contract aurora testnet

--- scripts/aurora/generate_template_testnet.sh
+++ scripts/aurora/generate_template_testnet.sh
@@ -5,6 +5,6 @@ set -e
 # prepare template 
 rm ./subgraph.yaml || true 
 cp ./subgraph_template.yaml ./subgraph.yaml 
-sed -i '' -e "s/{{ADDRESS}}/0xA1f8149E794b1Ca34a1cA6cbd9e735f48c494bc2/" ./subgraph.yaml 
+sed -i '' -e "s/{{ADDRESS}}/0x0a40db4b66ee040e74b89f1d03d42442d08e5014/" ./subgraph.yaml 
 sed -i '' -e "s/{{START_BLOCK}}/83441542/" ./subgraph.yaml
 sed -i '' -e "s/{{CHAIN}}/aurora-testnet/" ./subgraph.yaml
